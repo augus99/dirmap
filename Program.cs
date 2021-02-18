fee21c68b8fc767395e4f791707a97987b79886a
augus99 <augustocastelo1999@gmail.com>
2021-02-18 15:24:31 -0300

    Add argument validation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
+using System;
+using System.IO;
+using CommandLine;
+
 namespace Dirmap {
     public class Program {
         public static void Main(string[] args) {
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => {
+                if(!Directory.Exists(o.Target)) Exit("The specified directory cannot be found.");
+                else if(o.Depth <= 0) Exit("Depth value cannot be less nor equal to zero.");
+                else {
+                    // Safe to do the actual stuff
+                }
+            });
+        }
 
+        public static void Exit(string errorMessage) {
+            ConsoleColor prev = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\nError: ");
+            Console.ForegroundColor = prev;
+            Console.WriteLine(errorMessage);
+            Environment.Exit(-1);
         }
     }
 }
